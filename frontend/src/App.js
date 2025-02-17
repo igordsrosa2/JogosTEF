141a985c8e214eff52ad23782d6206bfdfd64c84
Igor dos Santos Rosa <igordsrosaa@gmail.com>
2025-02-17 15:54:37 -0300

    inserindo card de estatisticas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,16 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
+const StatsCard = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  width: 95%;
+  text-align: center;
+  max-width: 800px;
+`;
+
 const Container = styled.div`
   width: 100%;
   max-width: 800px;
@@ -36,14 +46,37 @@ const Subtitle = styled.p`
 function App() {
   const [jogos, setJogos] = useState([]);
   const [onEdit, setOnEdit] = useState([null]);
+  const [stats, setStats] = useState({ vitorias: 0, derrotas: 0, empates: 0 });
 
   const getJogos = async () => {
     try {
       const res = await axios.get("http://localhost:8800");
-      setJogos(res.data.sort((a, b) => (a.adversario > b.adversario ? 1 : -1)));
+      const sortedJogos = res.data.sort((a, b) =>
+        a.adversario > b.adversario ? 1 : -1
+      );
+      setJogos(sortedJogos);
+      calculateStats(sortedJogos);
     } catch (error) {
       toast.error(error);
     }
+  };
+
+  const calculateStats = (jogos) => {
+    let vitorias = 0,
+      derrotas = 0,
+      empates = 0;
+
+    jogos.forEach((jogo) => {
+      if (jogo.resultado === "Vitória") {
+        vitorias++;
+      } else if (jogo.resultado === "Derrota") {
+        derrotas++;
+      } else if (jogo.resultado === "Empate") {
+        empates++;
+      }
+    });
+
+    setStats({ vitorias, derrotas, empates });
   };
 
   useEffect(() => {
@@ -59,6 +92,14 @@ function App() {
         </Subtitle>
         <Form onEdit={onEdit} setOnEdit={setOnEdit} getJogos={getJogos} />
         <Grid jogos={jogos} setJogos={setJogos} setOnEdit={setOnEdit} />
+
+        {/* Card de Estatísticas */}
+        <StatsCard>
+          <h3>Estatísticas</h3>
+          <p>Vitórias: {stats.vitorias}</p>
+          <p>Derrotas: {stats.derrotas}</p>
+          <p>Empates: {stats.empates}</p>
+        </StatsCard>
       </Container>
       <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
